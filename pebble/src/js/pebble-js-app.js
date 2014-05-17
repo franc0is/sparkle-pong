d000830dbd23a392d77d7aa013f2a7f744a26a0e
franc0is <francois@getpebble.com>
2014-05-16 22:07:04 -0700

    get it to a working stage

--- pebble/src/js/pebble-js-app.js
+++ pebble/src/js/pebble-js-app.js
@@ -1,5 +1,105 @@
+var deviceId = localStorage.getItem("spark_id");
+var accessToken = localStorage.getItem("spark_token");
+
+/**************** Pebble helpers ****************/
+
+var hasKey = function(dict, key) {
+  return typeof dict.payload[key] !== "undefined";
+};
+
+var getValue = function(dict, key) {
+  if(hasKey(dict, key)) {
+    return dict.payload[key];
+  } else {
+    console.log("ERROR: Key '" + key + "' does not exist in received dictionary");
+    return undefined;
+  }
+};
+
+/************** Configuration *******************/
+Pebble.addEventListener("showConfiguration", function() {
+  console.log("showing configuration");
+  Pebble.openURL('http://francois-mbp.hq.getpebble.com:8000/configurable.html');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  console.log("configuration closed");
+  // webview closed
+  console.log("response : " + e.reponse);
+  var options = JSON.parse(decodeURIComponent(e.response));
+  accessToken = options.spark_token;
+  deviceId = options.spark_id;
+  console.log("id : " + deviceId);
+  console.log("token : " + accessToken);
+  localStorage.setItem("spark_id", deviceId);
+  localStorage.setItem("spark_token", accessToken);
+});
+
+/****************** XHR *************************/
+
+var success = function(json) {
+  console.log("Success " + json.result);
+  Pebble.sendAppMessage( {"SCORE": json.result} );
+};
+
+var failure = function() {
+  console.log("Failure");
+};
+
+var urlForPlayer = function(player) {
+  if (player > 2) {
+    console.log("This is a two player game!!");
+  }
+  return ("https://api.spark.io/v1/devices/" + deviceId +
+      "/p" + player + "?access_token=" + accessToken);
+}
+
+/****************** Main ************************/
+
 Pebble.addEventListener("ready",
-    function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
+  function(e) {
+    console.log("Pebble JS ready!");
+  }
+);
+
+Pebble.addEventListener("appmessage",
+  function(dict) {
+    var url = "";
+    var args = "";
+
+    if (hasKey(dict, "UP")) {
+      url = urlForPlayer(getValue(dict, "UP"));
+      args = "u";
+    } else if (hasKey(dict, "DOWN")) {
+      url = urlForPlayer(getValue(dict, "DOWN"));
+      args = "d";
+    } else if (hasKey(dict, "RESET")) {
+      url = "https://api.spark.io/v1/devices/" + deviceId +
+      "/reset?access_token=" + accessToken;
+    } else if (hasKey(dict, "SCORE")) {
+      url = "https://api.spark.io/v1/devices/" + deviceId +
+      "/score?access_token=" + accessToken;
+
+      ajax({
+        method: "get",
+        url: url,
+        type: "json"
+      }, success, failure);
+
+      return;
+    } else {
+      console.log("Invalid key received!");
+      return;
     }
+
+    console.log("ajax, url: " + url + ", args: " + args);
+
+    ajax({
+      method: "post",
+      url: url,
+      data: {"args": args},
+      type: "json"
+    });
+
+  }
 );
--- pebble/src/pebble.c
+++ pebble/src/pebble.c
@@ -1,60 +1,186 @@
 #include <pebble.h>
 
-static Window *window;
-static TextLayer *text_layer;
+/* Types */
+typedef enum {
+  PlayerOne = 1,
+  PlayerTwo = 2
+} PlayerId;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+typedef enum {
+  ActionUp,
+  ActionDown,
+  ActionReset,
+  ActionScore
+} Action;
+
+/* Player Selection Window */
+static Window *start_window;
+static TextLayer *start_p1_text_layer;
+static TextLayer *start_p2_text_layer;
+static TextLayer *start_title_text_layer;
+
+/* Game Window */
+static Window *game_window;
+static ActionBarLayer *action_bar;
+static PlayerId current_player;
+static GBitmap *reset_bitmap;
+static GBitmap *arrow_up_bitmap;
+static GBitmap *arrow_down_bitmap;
+static TextLayer *score_text_layer;
+static char score_text[6] = "0-0";
+static AppTimer *score_timer;
+
+static void send_action(Action action) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  Tuplet action_tuple = TupletInteger(action, current_player);
+  dict_write_tuplet(iter, &action_tuple);
+  app_message_outbox_send();
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+static void score_timer_callback(void *context) {
+  send_action(ActionScore);
+  score_timer = app_timer_register(1000, score_timer_callback, NULL);
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+static void game_up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send_action(ActionUp);
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+static void game_down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send_action(ActionDown);
 }
 
-static void window_load(Window *window) {
+static void game_select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send_action(ActionReset);
+}
+
+static void game_click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_UP, game_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, game_down_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, game_select_click_handler);
+}
+
+static void in_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *score_tuple = dict_find(iter, ActionScore);
+
+  if (score_tuple) {
+    strncpy(score_text, score_tuple->value->cstring, 6);
+    text_layer_set_text(score_text_layer, score_text);
+  }
+}
+
+static void game_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  app_message_register_inbox_received(in_received_handler);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  score_text_layer = text_layer_create(
+    (GRect) { .origin = { 42, 72 }, .size = { bounds.size.w, 20 } }
+  );
+  text_layer_set_text(score_text_layer, "0-0");
+  layer_add_child(window_layer, text_layer_get_layer(score_text_layer));
+  text_layer_set_font(score_text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+
+  action_bar = action_bar_layer_create();
+  action_bar_layer_add_to_window(action_bar, window);
+  action_bar_layer_set_click_config_provider(action_bar, game_click_config_provider);
+
+  arrow_up_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ARROW_UP);
+  arrow_down_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ARROW_DOWN);
+  reset_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RESET);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, arrow_up_bitmap);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, arrow_down_bitmap);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT, reset_bitmap);
+
+  score_timer = app_timer_register(1000, score_timer_callback, NULL);
 }
 
-static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+static void game_window_unload(Window *window) {
+  if (score_timer) {
+    app_timer_cancel(score_timer);
+  }
+  action_bar_layer_destroy(action_bar);
+  gbitmap_destroy(arrow_up_bitmap);
+  gbitmap_destroy(arrow_down_bitmap);
+  gbitmap_destroy(reset_bitmap);
+}
+
+static void start_game(PlayerId player_id) {
+  current_player = player_id;
+  game_window = window_create();
+  window_set_click_config_provider(game_window, game_click_config_provider);
+  window_set_window_handlers(game_window, (WindowHandlers) {
+    .load = game_window_load,
+    .unload = game_window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(game_window, animated);
+}
+
+static void start_up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  start_game(PlayerOne);
+}
+
+static void start_down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  start_game(PlayerTwo);
+}
+
+static void start_click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_UP, start_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, start_down_click_handler);
+}
+
+static void start_window_load(Window *window) {
+  Layer *start_window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(start_window_layer);
+
+  start_title_text_layer = text_layer_create(
+    (GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } }
+  );
+  start_p1_text_layer = text_layer_create(
+    (GRect) { .origin = { 0, 24 }, .size = { bounds.size.w, 20 } }
+  );
+  start_p2_text_layer = text_layer_create(
+    (GRect) { .origin = { 0, 120 }, .size = { bounds.size.w, 20 } }
+  );
+
+  text_layer_set_text(start_title_text_layer, "Sparkle Pong");
+  text_layer_set_text(start_p1_text_layer, "Player 1");
+  text_layer_set_text(start_p2_text_layer, "Player 2");
+
+  text_layer_set_text_alignment(start_title_text_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(start_p1_text_layer, GTextAlignmentRight);
+  text_layer_set_text_alignment(start_p2_text_layer, GTextAlignmentRight);
+
+  layer_add_child(start_window_layer, text_layer_get_layer(start_title_text_layer));
+  layer_add_child(start_window_layer, text_layer_get_layer(start_p1_text_layer));
+  layer_add_child(start_window_layer, text_layer_get_layer(start_p2_text_layer));
+}
+
+static void start_window_unload(Window *window) {
+  text_layer_destroy(start_p1_text_layer);
+  text_layer_destroy(start_p2_text_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
+  app_message_open(32, 32);
+  start_window = window_create();
+  window_set_click_config_provider(start_window, start_click_config_provider);
+  window_set_window_handlers(start_window, (WindowHandlers) {
+    .load = start_window_load,
+    .unload = start_window_unload,
   });
   const bool animated = true;
-  window_stack_push(window, animated);
+  window_stack_push(start_window, animated);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+  window_destroy(start_window);
 }
 
 int main(void) {
   init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
   app_event_loop();
   deinit();
 }
